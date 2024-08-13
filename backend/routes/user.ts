a94969e98f87597f23dbcbf8aed0d4cb316071a9
syedubaid <syedabuubaid@gmail.com>
2024-08-14 01:07:25 +0530

    commit

---
+++ backend/routes/user.ts
@@ -0,0 +1,8 @@
+import {decode,sign,verify} from 'hono/jwt'
+import { Hono } from 'hono'
+
+const userRouter=new Hono();
+
+
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'hono'
-
 const app = new Hono()
 
 app.get('/', (c) => {
@@ -10,6 +9,10 @@ app.post('/api/v1/signup',(c)=>{
   return c.text("this is the signup route")
 })
 
+app.post('/api/v1/sigin',async (c)=>{
+  const {username,passowrd}=await c.req.json();
+  
+})
 
 
 export default app
