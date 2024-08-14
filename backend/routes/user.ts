f11c5d5c29de2ebf566068efb3f7f7e6a77c17ac
syedubaid <syedabuubaid@gmail.com>
2024-08-15 00:32:25 +0530

    user route

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -3,6 +3,8 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { env } from 'hono/adapter';
+import { z } from "zod";
+
 
 export const userRouter=new Hono<{Bindings:
     {DATABASE_URL:string;
@@ -10,18 +12,40 @@ export const userRouter=new Hono<{Bindings:
 
 }>();
 
-userRouter.post('/signup',async (c)=>{
-        const prisma=new PrismaClient({
-            datasourceUrl:c.env.DATABASE_URL,
-        }).$extends(withAccelerate());
-        const body=c.req.json()
-        
-        const user=await prisma.user.findUnique({
-            where:{
-                email:body.email
-            }
+const signupBody=z.object({
+    email:z.string(),
+    firstName:z.string(),
+    lastName:z.string(),
+    password:z.string()
+})
 
-        })
+userRouter.post('/signup',async (c)=>{
+       const requestBody=await c.req.text;
+       const {success}=signupBody.safeParse(c.body)
+        if(success){
+            const prisma=new PrismaClient({
+                datasourceUrl:c.env.DATABASE_URL,
+            }).$extends(withAccelerate());
+            const body=c.req.json()
+            try{
+                const user=await prisma.user.findUnique({
+                    where:{
+                        email:body.email
+                    }
+                })
+            }
+            catch(err){
+                return c.text(`${err}`);
+            }
+            
+        }
+        else{
+            return c.text(`The incoming details are : ${requestBody}`)
+            return c.text("Invalid details")
+
+        }
+
+        
 
         
 })
