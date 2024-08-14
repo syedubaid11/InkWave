3e150baeb1c6308409fa8f0eccea9e607c62f89a
syedubaid <syedabuubaid@gmail.com>
2024-08-14 11:32:35 +0530

    commit

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,8 +1,24 @@
 import {decode,sign,verify} from 'hono/jwt'
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const userRouter=new Hono();
+export const userRouter=new Hono<{Bindings:
+    {DATABASE_URL:string;
+    JWT_SECRET:string}
+
+}>();
+
+userRouter.post('/api/v1/signup',async (c)=>{
+    const prisma=await new PrismaClient({
+        datasourceUrl:c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    if(prisma){
+        return c.text 
+    }
 
 
+})
 
 
