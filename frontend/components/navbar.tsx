99ce1158647f1a14875f3cc4193dec19a0cefa5f
syedubaid <syedabuubaid@gmail.com>
2024-08-22 01:24:56 +0530

    commit

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const Navbar=()=>{
+    return(
+        <div className="flex justify-between h-10 border-2">
+            <div>
+                Ink Wave
+            </div>
+            <div>
+                the modern blog
+            </div>
+
+
+        </div>
+    )
+
+}
---
+++ frontend/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Homepage=()=>{
+    return(
+        <div>
+            <h1>InkWave</h1>
+        </div>
+    )
+
+}
