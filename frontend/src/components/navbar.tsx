53ad3ed613be24f0376e8c391aea87b20ee95977
syedubaid <syedabuubaid@gmail.com>
2024-08-23 00:16:30 +0530

    added framer motion

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,12 +1,12 @@
 export function Navbar(){
     return(
-        <div className="flex flex-row justify-between border-2 bg-gray-950 text-amber-300">
+        <div className="flex flex-row justify-between">
             <div className="m-4 text-4xl font-archivo-black p-4">
                 Inkwave.
             </div>
         <div className="m-4 p-4">
-            <button className="m-3 border p-1.5 rounded-md">Sign Up</button>
-            <button className="border p-1.5 rounded-md">Sign In</button>
+            <button className="m-3 border border-black font-archivo-black p-1.5 rounded-md ">Sign Up</button>
+            <button className="border border-black p-1.5 font-archivo-black rounded-md">Sign In</button>
         </div>
             
         </div>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,13 +1,26 @@
+import { motion } from "framer-motion"
 import { Navbar } from "../components/navbar";
+import { useEffect, useState } from "react";
 
 export function Home(){
+
+    const[isVisible,setVisible]=useState(false)
+
+    useEffect(()=>{
+        setVisible(true)
+    },[])
+
     return(
         <>
-        <Navbar/>
-
-
-
-        </>
-        
+          <motion.div
+            initial={{ opacity: 0, y: -50 }} // Start with opacity 0 and move upwards
+            animate={{ opacity: isVisible ? 1 : 0, y: 0 }} // Animate to opacity 1 and move to original position
+            transition={{ duration: 1 }} // Set duration for the transition
+        >
+        <div className="h-screen w-screen bg-amber-300">
+            <Navbar/>
+        </div>
+        </motion.div>
+        </>      
     )
 }
