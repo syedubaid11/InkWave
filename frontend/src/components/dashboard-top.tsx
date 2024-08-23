69d120d59f686e2d1505048fdff48ce032e11a09
syedubaid <syedabuubaid@gmail.com>
2024-08-24 02:14:32 +0530

    commit

--- frontend/src/components/dashboard-top.tsx
+++ frontend/src/components/dashboard-top.tsx
@@ -10,15 +10,15 @@ export function DashboardTop(){
         axios.get('http://localhost:8787/api/v1/blog/bulk')
     })
     return(
-        <div className="flex flex-row justify-between border-2 p-4 m-4">
-        <div className="flex">
-            <p className="text-4xl font-archivo-black">Inkwave</p>
-            <input className="ml-20" type="text" placeholder="Search a blog"></input>
-        </div>
+        <div className="flex flex-row justify-between p-2 m-2 border-b border-slate-900">
+            <div className="flex items-center">
+                <p className="text-2xl md:text-4xl font-archivo-black">Inkwave</p>
+                <input className="h-fit rounded-md ml-4 border-2 w-28 md:ml-8 md:w-fit " type="text" placeholder="Search a blog"></input>
+            </div>
+            <div className="flex items-center">
+                <button className="border-2 rounded-lg p-2 text-sm md:text-xl">✍️</button>
+            </div>
 
-        <div className="flex items-center">
-            <button className="border-2">Write a blog</button>
-        </div>
         </div>
     )
 }
